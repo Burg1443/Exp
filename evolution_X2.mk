87d1ac8ec8d31a28227eade1c77ca23cc5d0d165
RjTangpos <rjtangpos39@gmail.com>
2023-03-29 11:55:51 +0800

    X2: Bring up Evolution

--- evolution_X2.mk
+++ evolution_X2.mk
@@ -10,31 +10,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from X2 device
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some Spark stuff.
+# Inherit some Evolution stuff.
+EVO_BUILD_TYPE := OFFICIAL
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-SPARK_BUILD_TYPE := UNOFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
-# Inherit some props from Spark
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.spark.maintainer= ☠️☠𝕽𝕵𝕯𝕬𝕽𝕶❸❾☠☠️🏴‍
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_X2
+PRODUCT_NAME := evolution_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
 PRODUCT_MANUFACTURER := realme
 
-BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys"
+BUILD_FINGERPRINT := "realme/RMX1993/RMX1993L1:11/RKQ1.201112.002/1639416071279:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
